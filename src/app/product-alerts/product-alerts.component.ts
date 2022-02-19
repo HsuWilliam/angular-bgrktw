9bdfdf390bbdd2d37245ae770723a643b014ead3
HsuWilliam <hsu870422@gmail.com>
2022-02-19 17:46:28 +0800

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -9,7 +9,7 @@ import { products } from '../products';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-  
+
   @Output() notify = new EventEmitter();
   constructor() { }
 
